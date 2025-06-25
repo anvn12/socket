34e69ad37affbd6c693c085e0aa776593b25b490
anvn12 <anmega83@gmail.com>
2025-06-25 23:01:31 +0700

    them ham main co ban

--- socket-client/socket-client/socket-client.cpp
+++ socket-client/socket-client/socket-client.cpp
@@ -2,8 +2,20 @@
 
 int main() {
 
+	WSADATA wsaData;
+
+	// Initialize Winsock
+	const int startupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (startupResult != 0) { // fail to start up
+		printf("WSAStartup failed: %d\n", startupResult);
+		return 1;
+	}
 
 
+
+
+	// cleanup Winsock
+	WSACleanup();
 
 	return 0;
 }
